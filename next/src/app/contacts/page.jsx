9466ea2bf78beb1bb50ed7eb3b9c68f02a5decd2
piyush7833 <96736508+piyush7833@users.noreply.github.com>
2023-07-31 22:57:26 +0530

    contacts

--- next/src/app/contacts/page.jsx
+++ next/src/app/contacts/page.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import styles from './page.module.css'
+import Btn from '@/components/btn/Btn'
+import Image from 'next/image'
 const Contacts = () => {
   return (
     <div className={styles.container}>
-      Contacts
+      <div className={styles.title}>
+        <h1>Lets Keep in Touch.</h1>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image className={styles.img} src="/contact.png" alt="contact" fill={true}/>
+        </div>
+        <div className={styles.inputContainer}>
+          <input className={styles.input} type="text" name="" placeholder='name' id="" />
+          <input className={styles.input} type="text" name="" placeholder='email' id="" />
+          <textarea className={styles.textarea} cols={30} rows={10} placeholder='message' />
+          <Btn url="" text="Send"/>
+        </div>
+      </div>
     </div>
   )
 }
--- next/src/app/portfolio/page.jsx
+++ next/src/app/portfolio/page.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import styles from './page.module.css'
+import Link from 'next/link'
 const Portfolio = () => {
   return (
     <div className={styles.container}>
-      Portfolio
+      <h1 className={styles.selectTitle}>Choose a gallery</h1>
+      <div className={styles.item}>
+        <Link className={styles.item} href="/portfolio/illustration" >
+          <span className={styles.title}>Illustration</span>
+        </Link>
+        <Link className={styles.item} href="/portfolio/websites" >
+          <span className={styles.title}>Illustration</span>
+        </Link>
+        <Link className={styles.item} href="/portfolio/applications" >
+          <span className={styles.title}>Illustration</span>
+        </Link>
+      </div>
     </div>
   )
 }
